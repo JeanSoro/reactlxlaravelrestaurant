a4b8d195de53497101c4bc5dfbb8e475385cac76
Jean <jean.soro007@gmail.com>
2020-03-21 12:47:14 -0400

    Transfer static htmlcss files  to Laravel and set up browsersync

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,92 +8,232 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/9dac89efb2.js" crossorigin="anonymous"></script>
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="/css/app.css">
+        
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <section id="welcome">
+            <div class="welcome-menu">
+              <div class="logo">
+                <img src="https://webstockreview.net/images/clipart-restaurant-restaurant-logo-5.png">
+              </div>
+              <div class="menu">
+                <div class="menu-title">
+                  Menu
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <ul class="links">
+                  <li>
+                    <a href="#">Menu</a>
+                  </li>
+                  <li>
+                    <a href="#">Waitlist</a>
+                  </li>
+                  <li>
+                    <a href="#">Offers</a>
+                  </li>
+                  <li>
+                    <a href="#">Giftcards</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="social-icons">
+                <a href="#">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+              </div>
+              <div class="location">
+                <div class="address">
+                  234 Main st,<br>
+                  New York, New York 11747
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="phone-number">
+                  <a href="tel:7182198652">718-219-8652</a>
                 </div>
+              </div>
             </div>
-        </div>
+            <div class="welcome-jumbo">
+              <div class="status">NEW</div>
+              <h1>Billy Burger</h1>
+              <img src="img/hamburger-and-fries-hero.png" class="burger-fries">
+            </div>
+          </section>
+          <section id="information">
+            <div class="restaurant-image">
+              <img src="img/top-down.jpg">
+            </div>
+            <div class="info">
+              <h2>We Started From The Bottom In 1987</h2>
+              <div class="paragraphs">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad officia
+                  fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+                  et
+                  similique!Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad
+                  officia
+                  fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+                  et
+                  similique!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad officia
+                  fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+                  et
+                  similique!Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad
+                  officia
+                  fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+                  et
+                  similique!</p>
+              </div>
+              <a href="#" class="about-link">
+                <span>Learn more about restaurants</span>
+                <div class="circle-right"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+              </a>
+            </div>
+          </section>
+          <section id="food-preview">
+            <h2>We have everything you need to kill your hunger</h2>
+            <div class="button-rounded">View Our Menu</div>
+            <div class="container">
+              <div class="left-btn">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+              </div>
+              <div class="right-btn">
+                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+              </div>
+              <div class="food-slider">
+                <div class="sliding-system">
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Starters
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img src="img/chips.png">
+                      </div>
+                    </div>
+        
+                  </div>
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Burgers
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img src="img/hamburger-and-fries.png">
+                      </div>
+                    </div>
+        
+                  </div>
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Entrees
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img src="img/pork-entrees-steak-pork.png">
+                      </div>
+                    </div>
+        
+                  </div>
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Sides
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img src="img/salad.png">
+                      </div>
+                    </div>
+        
+                  </div>
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Deserts
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img
+                          src="https://pngriver.com/wp-content/uploads/2018/04/Download-Cupcake-PNG-Transparent-Image-420x190.png">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Beers
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img
+                        src="img/cupcakes.png">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="slide">
+                    <div class="background"></div>
+                    <div class="content">
+                      <div class="food-title">
+                        Drinks
+                      </div>
+                      <p class="food-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                        architecto
+                        officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                        repellendus natus nihil?
+                      </p>
+                      <div class="food-image">
+                        <img
+                          src="img/cupcakes.png">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        
+          </section>
     </body>
 </html>
