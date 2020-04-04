69360a8a9be86f11880f936bedf97a83df93364d
Jean <jean.soro007@gmail.com>
2020-04-04 13:47:58 -0400

    Created roles and migrations as well as many to many relationships between User and Role Models

---
+++ database/migrations/2020_04_04_173608_create_role_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('role_id')->unsigned()->index();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->timestamps();
+        });
+
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->foreign('role_id')->references('id')->on('roles')->onCascade('delete');
+            $table->foreign('user_id')->references('id')->on('users')->onCascade('delete');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_user');
+    }
+}
