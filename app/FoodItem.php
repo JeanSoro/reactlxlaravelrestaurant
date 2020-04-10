3016d9e35f73c4878332d62bf2c29ad9dfd72a79
Jean <jean.soro007@gmail.com>
2020-04-10 16:43:13 -0400

    Created models and migrations for fooditems and foodcategories

---
+++ app/FoodItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FoodItem extends Model
+{
+
+    protected $table = 'food_items';
+
+    public function food_categories(){
+        return $this->belongsTo('App\FoodCategory');
+    }
+    
+}
