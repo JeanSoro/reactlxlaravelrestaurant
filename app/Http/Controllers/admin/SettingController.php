0ae7170e2302f80ee52381bbc0340b874e83944e
Jean <jean.soro007@gmail.com>
2020-04-18 11:46:15 -0400

    finished creating the forms for the general view related to general settings to allow editing of information in the backend

--- app/Http/Controllers/admin/SettingController.php
+++ app/Http/Controllers/admin/SettingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Setting;
+use App\GeneralSetting;
 
 class SettingController extends Controller
 {
@@ -14,30 +14,44 @@ class SettingController extends Controller
     }
 
 
-    public function edit($id){
+    public function general(){
 
-        $category = FoodCategory::find($id);
-        return view('admin/food-categories/edit', [
-            'category'=> $category
+        $id = 1;
+
+        $general_setting = GeneralSetting::find($id);
+        return view('admin/settings/general', [
+            'general_setting'=> $general_setting
         ]);
     }
 
-    public function update($id){
+    public function saveGeneral(){
+
+        $id = 1;
 
          request()->validate([
-            'title' => ['required', 'string', 'max:255'],
-            'description' => ['required', 'string'],
-            'image_url' => ['required', 'string'],
-            
+            'site_title' => ['required', 'string', 'max:255'],
+            'logo_image_url' => ['required', 'string'],
+            'address_1' => ['required', 'string'],
+            'address_2' => ['string'],
+            'city' => ['required, string'],
+            'province' => ['required, string'],
+            'postal_code' => ['required, string'],
+            'phone_number' => ['required, string'],
+          
         ]);
 
-        $category = FoodCategory::find($id);
-        $category->title = request('title');
-        $category->description = request('description');
-        $category->image_url = request('image_url');
-        $category->save();
+        $general_setting = GeneralSetting::find($id);
+        $general_setting->site_title = request('site_title');
+        $general_setting->logo_image_url = request('logo_image_url');
+        $general_setting->address_1 = request('address_1');
+        $general_setting->address_2 = request('address_2');
+        $general_setting->city = request('city');
+        $general_setting->province = request('province');
+        $general_setting->postal_code = request('postal_code');
+        $general_setting->phone_number = request('phone_number');
+        $general_setting->save();
 
-        return redirect('/admin/food-categories');
+        return redirect('/admin/settings/general');
 
     }
 
--- resources/views/admin/settings/general.blade.php
+++ resources/views/admin/settings/general.blade.php
@@ -9,14 +9,13 @@
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="page-header">
-                    <h2 class="pageheader-title">New Food Category </h2>
+                    <h2 class="pageheader-title">Edit General setting </h2>
                     <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                     <div class="page-breadcrumb">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="/admin/food-categories" class="breadcrumb-link">All Food Categories</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">New Food Category</li>
+                                <li class="breadcrumb-item active" aria-current="page">Edit General settings</li>
                             </ol>
                         </nav>
                     </div>
@@ -26,70 +25,99 @@
         <!-- ============================================================== -->
         <!-- end pageheader -->
         <!-- ============================================================== -->
- 
-            <div class="row">
-                <!-- ============================================================== -->
-                <!-- basic form -->
-                <!-- ============================================================== -->
-                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                    <div class="card">
-                        <h5 class="card-header">Create a new category</h5>
-                        <div class="card-body">
-                            <form method="POST" action="/admin/food-categories">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="inputTitle">Title</label>
+        <div class="row">
+            <!-- ============================================================== -->
+            <!-- basic form -->
+            <!-- ============================================================== -->
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                <div class="card">
+                    <h5 class="card-header">Edit General setting</h5>
+                    <div class="card-body">
+                        <form method="POST" action="/admin/settings/general">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="inputTitle">Business Name</label>
+                                <input id="inputTitle" type="text" class="form-control form-control-lg @error('site_title') is-invalid @enderror" name="site_title" value="{{ old('site_title', $general_setting->site_title) }}" required autocomplete="site_title" autofocus placeholder="site title">
+                                @error('site_title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+                            <div class="form-group">
+                                <label for="inputAddress1">Main Location</label>
+                                <input id="inputAddress1" type="text" class="form-control form-control-lg @error('address_1') is-invalid @enderror" name="address_1" value="{{ old('address_1', $general_setting->address_1) }}" required autocomplete="address_1" autofocus placeholder="Add main address">
+                                @error('address_1')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+                            <div class="form-group">
+                                <label for="inputAddress2">Apt/Suite/ect...</label>
+                                <input id="inputAddress2" type="text" class="form-control form-control-lg @error('address_2') is-invalid @enderror" name="address_2" value="{{ old('address_2', $general_setting->address_2) }}" required autocomplete="address_2" autofocus placeholder="Add Apt/Suite/ect... number">
+                                @error('address_2')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+                            <div class="form-group">
+                                <label for="inputCity">City</label>
+                                <input id="inputCity" type="text" class="form-control form-control-lg @error('city') is-invalid @enderror" name="city" value="{{ old('city', $general_setting->city) }}" required autocomplete="city" autofocus placeholder="Add a city">
+                                @error('city')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+                            <div class="form-group">
+                                <label for="inputProvince">Province</label>
+                                <input id="inputProvince" type="text" class="form-control form-control-lg @error('province') is-invalid @enderror" name="province" value="{{ old('province', $general_setting->province) }}" required autocomplete="province" autofocus placeholder="Add a province">
+                                @error('province')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPostalCode">Postal Code</label>
+                                <input id="inputPostalCode" type="text" class="form-control form-control-lg @error('postal_code') is-invalid @enderror" name="postal_code" value="{{ old('postal_code', $general_setting->postal_code) }}" required autocomplete="postal_code" autofocus placeholder="Add a postal code">
+                                @error('postal_code')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPhoneNumber">Phone Number</label>
+                                <input id="inputPhoneNumber" type="tel" class="form-control form-control-lg @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number', $general_setting->phone_number) }}" required autocomplete="phone_number" autofocus placeholder="Add a phone number">
+                                @error('phone_number')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror       
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
                                     
-                                    <input id="inputTitle" type="text" class="form-control form-control-lg @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus placeholder="title">
-                                    @error('title')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror       
                                 </div>
-
-                                <div class="form-group">
-                                    <label for="inputImageUrl">Image URL</label>
-                                    
-                                    <input id="inputImageUrl" type="text" class="form-control form-control-lg @error('image_url') is-invalid @enderror" name="image_url" value="{{ old('image_url') }}" required autocomplete="image_url" autofocus placeholder="image_url">
-                                    @error('image_url')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror       
+                                <div class="col-sm-6 pl-0">
+                                    <p class="text-right">
+                                        <button type="submit" class="btn btn-space btn-primary">Submit</button>
+                                    </p>
                                 </div>
-
-                                <div class="form-group">
-                                    <label for="inputDescription">Description</label>
-
-                                    <textarea id="inputDescription" type="text" class="form-control form-control-lg @error('description') is-invalid @enderror" name="description" autofocus placeholder="description">
-                                        {{ old('description') }}
-                                    </textarea>
-                                    @error('description')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror    
-                                </div>
-                                
-
-                                <div class="row">
-                                    <div class="col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
-                                        
-                                    </div>
-                                    <div class="col-sm-6 pl-0">
-                                        <p class="text-right">
-                                            <button type="submit" class="btn btn-space btn-primary">Submit</button>
-                                        </p>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <!-- ============================================================== -->
-                <!-- end basic form -->
-                <!-- ============================================================== -->
             </div>
+            <!-- ============================================================== -->
+            <!-- end basic form -->
+            <!-- ============================================================== -->
+        </div>
     </div>
 @endsection
+
