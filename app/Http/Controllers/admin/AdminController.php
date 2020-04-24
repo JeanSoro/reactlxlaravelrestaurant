83f8aefb58be24b51cae07bcb0825b64dad3d082
Jean <jean.soro007@gmail.com>
2020-04-24 15:42:53 -0400

    calculated revenue for last thirty days and displayed on dashboard using AdminController

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Member;
+use App\Reservation;
+use DB;
 
 class AdminController extends Controller
 {
@@ -15,6 +19,21 @@ class AdminController extends Controller
 
 
     public function dashboard(){
-        return view('admin/dashboard');
+
+       $estimated_guests_last_thirty_days = DB::select(DB::raw('
+
+            SELECT 
+                sum(guests_total) as total_guests_in_month
+            FROM restaurant.reservations
+            WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+
+        '));
+
+        $estimated_guests_last_thirty_days = $estimated_guests_last_thirty_days[0]->total_guests_in_month;
+
+
+        return view('admin/dashboard', [
+            "estimated_guests_last_thirty_days" => $estimated_guests_last_thirty_days
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,9 +32,9 @@
               <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
                   <div class="card">
                       <div class="card-body">
-                          <h5 class="text-muted">Total Revenue</h5>
+                          <h5 class="text-muted">Revenue Last 30 Days</h5>
                           <div class="metric-value d-inline-block">
-                              <h1 class="mb-1">$12099</h1>
+                              <h1 class="mb-1">${{$estimated_guests_last_thirty_days * 30}}</h1>
                           </div>
                           <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                               <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
