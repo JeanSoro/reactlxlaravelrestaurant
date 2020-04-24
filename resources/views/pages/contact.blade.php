151d155bd6c8780bc1b9ac8cf08ee9fb55ecb6ec
Jean <jean.soro007@gmail.com>
2020-04-24 12:21:44 -0400

    embeded google maps location to contact page view

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -26,7 +26,7 @@ Mdrn Bites was established in 2019 and is the hot new spot for all crowds, young
                 </p>
             </div>
             <div class="col-md-6">
-                <img src="img/people-drinking-liquor.jpg" class="img-fluid" alt="">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2797.810999408982!2d-73.62636074850084!3d45.473611078998516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9174d3de566cb%3A0xaefdc26e55200b37!2sTyphoon%20Lounge!5e0!3m2!1sen!2sca!4v1587745236384!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
       </div>
