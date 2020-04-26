327797bb9858610c0800ea4b3bbc211a7ff4589d
Jean <jean.soro007@gmail.com>
2020-04-26 14:19:22 -0400

    Added functionality for slider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,5 +15,71 @@
                 @yield('content')
             </section>
           </section>
+          <script src="/assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+
+  <script>
+    let slides = $('.sliding-system a.slide').length;
+    let maxSlides;
+    let moveTotal;
+    let currentSlide = 0;
+
+    $(window).resize(function(){
+      currentSlide = 0;
+      $('.sliding-system').css('transform', 'translate3d(-0%,0,0)');
+    })
+
+    function desktopSlider(a){
+      if(a.matches){
+        maxSlides = Math.ceil((slides * 20) / 100);
+        moveTotal = 100;
+
+        $('.right-btn').on('click', () => {
+          if(currentSlide < maxSlides){
+            currentSlide += 100;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%,0,0)`);
+          }
+        });
+
+        $('.left-btn').on('click', () => {
+          if(currentSlide != 0){
+            currentSlide -= 100;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%,0,0)`);
+          }
+        });
+      }
+    }
+
+    let a = window.matchMedia("(min-width:998px)");
+    desktopSlider(a);
+    a.addListener(desktopSlider);
+
+
+    function tabletSlider(b){
+      if(b.matches){
+        maxSlides = Math.ceil((slides * 50) / 100);
+        moveTotal = 100;
+
+        $('.right-btn').on('click', () => {
+          if(currentSlide < ((maxSlides * 100) - 100)){
+            currentSlide += 50;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%,0,0)`);
+          }
+        });
+
+
+        $('.left-btn').on('click', () => {
+          if(currentSlide != 0){
+            currentSlide -= 50;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%,0,0)`);
+          }
+        });
+      }
+    }
+
+     let b = window.matchMedia("(min-width:768px) and (max-width: 997px)");
+    tabletSlider(b);
+    b.addListener(tabletSlider);
+
+  </script>
     </body>
 </html>
